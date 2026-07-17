ae007fa72a616f3bf750d53d318dc483b876de4d
Karan Kumar Sharma <sk5485633@gmail.com>
2026-07-18 01:46:18 +0530

    Basic setup like firebase authentaction implemented for login

---
+++ cotexAI/frontend/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { signInWithPopup } from 'firebase/auth'
+import React from 'react'
+import { auth, googleProvider } from '../utils/firebase'
+
+const App = () => {
+
+  const googleSignIn = async () => {
+    // Implement Google Sign-In logic here
+    const dataResponse = await signInWithPopup(auth, googleProvider);
+    console.log(dataResponse);
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <button className="w-50 h-24 bg-amber-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={googleSignIn}>
+        Continue with google
+      </button>
+    </div>
+  )
+}
+
+export default App
